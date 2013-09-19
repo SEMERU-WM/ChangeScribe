078b9750c3f3ecb9b7bb103507a17368ed7a7b47
luiscortes <luiscortes@gkudos.com>
2013-09-19 09:23:54 -0500

    minor change

---
+++ CommitSummarizer/CommitSummarizer.core/src/main/java/commitsummarizer/core/handlers/CreateMessageHandler.java
@@ -0,0 +1,34 @@
+package commitsummarizer.core.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class CreateMessageHandler extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public CreateMessageHandler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"CommitSummarizer Core Plugin",
+				"CreateMessage");
+		return null;
+	}
+}
