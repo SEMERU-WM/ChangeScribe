3235727d8e672dfda52b0a4f225bec72cc41c697
luiscortes <luiscortes@gkudos.com>
2013-10-10 06:44:12 -0500

    added first implementation of removed types

--- CommitSummarizer/CommitSummarizer.core/src/main/java/co/edu/unal/colswe/CommitSummarizer/core/summarizer/SummarizeChanges.java
+++ CommitSummarizer/CommitSummarizer.core/src/main/java/co/edu/unal/colswe/CommitSummarizer/core/summarizer/SummarizeChanges.java
@@ -1,6 +1,7 @@
 package co.edu.unal.colswe.CommitSummarizer.core.summarizer;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -9,7 +10,9 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -17,8 +20,14 @@ import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.errors.AmbiguousObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.RevisionSyntaxException;
 import org.eclipse.swt.widgets.Display;
 
 import co.edu.unal.colswe.CommitSummarizer.core.FilesChangedListDialog;
@@ -56,6 +65,7 @@ public class SummarizeChanges {
 		summarized = new TreeMap<String, StereotypeIdentifier>();
 		getChangedListDialog().getText().setText("");
 		String currentPackage = "";
+		removeCreatedPackages();
 		
 			Job job = new Job("Calculating method and types stereotypes") {
 				@Override
@@ -66,9 +76,10 @@ public class SummarizeChanges {
 							protected IStatus run(IProgressMonitor monitor) {
 								StereotypeIdentifier identifier = null;
 								try {
-									if(!file.getChangeType().equals(TypeChange.UNTRACKED.name())) {
+									System.out.println("CHANGE TYPE: " + file.getChangeType());
+									/*if(!file.getChangeType().equals(TypeChange.UNTRACKED.name())) {
 										File left = Utils.getFileContentOfLastCommit(file.getPath(), getGit().getRepository());
-										File right = new File(file.getAbsolutePath());
+										File right = new File(file.getAbsolutePath());*/
 										
 										/*if(file.getAbsolutePath().endsWith(".java")) {
 											distiller.extractClassifiedSourceCodeChanges(left, right);
@@ -81,15 +92,16 @@ public class SummarizeChanges {
 											}
 											
 										}*/
-									} else if(file.getChangeType().equals(TypeChange.UNTRACKED.name())) {
+									/*} else */
+									if(file.getChangeType().equals(TypeChange.UNTRACKED.name()) || file.getChangeType().equals(TypeChange.ADDED.name())) {
 										if(file.getAbsolutePath().endsWith(".java")) {
 											monitor.subTask("Identifying stereotypes for " + file.getName());
-											identifier = identifyStereotypes(file, "added");
+											identifier = identifyStereotypes(file, file.getChangeType());
 										} 
 									} else if(file.getChangeType().equals(TypeChange.REMOVED.name())) {
 										if(file.getAbsolutePath().endsWith(".java")) {
 											monitor.subTask("Identifying stereotypes for " + file.getName());
-											identifier= identifyStereotypes(file, "removed");
+											identifier = identifyStereotypes(file, file.getChangeType());
 										}
 									}
 								} catch(Exception e) {
@@ -147,7 +159,7 @@ public class SummarizeChanges {
 
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
-				if(summarized.size() == identifiers.size()) {
+				if(summarized.size() == differences.length) {
 					String currentPackage = "";
 					StringBuilder desc = new StringBuilder();
 					desc.append(summarizeCommitStereotype());
@@ -164,12 +176,25 @@ public class SummarizeChanges {
 						desc.append(identifier.getValue().toString());
 					}
 					getChangedListDialog().getText().setText(desc.toString());
+					removeCreatedPackages();
 				}
 			}
 		});
 
 	}
 	
+	
+	
+	protected void removeCreatedPackages() {
+		IFolder folder = ((IJavaProject)changedListDialog.getSelection()).getProject().getFolder("src/commsummtmp");
+		try {
+			folder.delete(true, null);
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public void summarizeType(StereotypeIdentifier identifier) {
 		for(StereotypedElement element : identifier.getStereotypedElements()) {
 				SummarizeType summarizeType = new SummarizeType(element, identifier, differences);
@@ -208,16 +233,42 @@ public class SummarizeChanges {
 		
 		String projectName = "";
 		IResource res = null;
-		if(changedListDialog.getSelection() != null) {
-			projectName = changedListDialog.getSelection().getElementName();
-			res = changedListDialog.getSelection().getProject().findMember(file.getPath().replaceFirst(projectName, ""));
-			stereotypeIdentifier = new StereotypeIdentifier((ICompilationUnit) JavaCore.create(res, changedListDialog.getSelection()), 0, 0);
-		} else {
-			projectName = ProjectInformation.getProject(ProjectInformation.getSelectedProject()).getName();
-			res = ProjectInformation.getProject(ProjectInformation.getSelectedProject()).findMember(file.getPath().replaceFirst(projectName, ""));
-			IFile ifile = ProjectInformation.getSelectedProject().getWorkspace().getRoot().getFile(res.getFullPath());
-			stereotypeIdentifier = new StereotypeIdentifier((ICompilationUnit) JavaCore.create(ifile), 0, 0);
+		if(scmOperation.equals(TypeChange.ADDED.toString()) ||scmOperation.equals(TypeChange.UNTRACKED.toString())) {
+			if(changedListDialog.getSelection() != null) {
+				projectName = changedListDialog.getSelection().getElementName();
+				res = changedListDialog.getSelection().getProject().findMember(file.getPath().replaceFirst(projectName, ""));
+				stereotypeIdentifier = new StereotypeIdentifier((ICompilationUnit) JavaCore.create(res, changedListDialog.getSelection()), 0, 0);
+			} else {
+				projectName = ProjectInformation.getProject(ProjectInformation.getSelectedProject()).getName();
+				res = ProjectInformation.getProject(ProjectInformation.getSelectedProject()).findMember(file.getPath().replaceFirst(projectName, ""));
+				IFile ifile = ProjectInformation.getSelectedProject().getWorkspace().getRoot().getFile(res.getFullPath());
+				stereotypeIdentifier = new StereotypeIdentifier((ICompilationUnit) JavaCore.create(ifile), 0, 0);
+			}
+		} else if(scmOperation.equals(TypeChange.REMOVED.toString())) {
+			try {
+				String removedFile = Utils.getStringContentOfLastCommit(file.getPath(), getGit().getRepository());
+				projectName = changedListDialog.getSelection().getElementName();
+
+				IPackageFragment pack = null;
+				IFolder folder = ((IJavaProject)changedListDialog.getSelection()).getProject().getFolder("src");
+				pack = changedListDialog.getSelection().getPackageFragmentRoot(folder).createPackageFragment("commsummtmp", true, null);
+				
+				ICompilationUnit cu = pack.createCompilationUnit(file.getName(), removedFile,false, null);
+				stereotypeIdentifier = new StereotypeIdentifier(cu, 0, 0);
+			} catch (RevisionSyntaxException e) {
+				e.printStackTrace();
+			} catch (AmbiguousObjectException e) {
+				e.printStackTrace();
+			} catch (IncorrectObjectTypeException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (JavaModelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
 		
 		stereotypeIdentifier.identifyStereotypes();
 		stereotypeIdentifier.setScmOperation(scmOperation);
--- CommitSummarizer/CommitSummarizer.core/src/main/java/co/edu/unal/colswe/CommitSummarizer/core/util/Utils.java
+++ CommitSummarizer/CommitSummarizer.core/src/main/java/co/edu/unal/colswe/CommitSummarizer/core/util/Utils.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.StringWriter;
 
 import org.eclipse.jgit.errors.AmbiguousObjectException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
@@ -18,6 +19,8 @@ import org.eclipse.jgit.revwalk.RevTree;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.treewalk.filter.PathFilter;
+
+import edu.stanford.nlp.io.IOUtils;
 
 public class Utils {
 
@@ -55,6 +58,38 @@ public class Utils {
 		return Utils.inputStreamToFile(loader.openStream());
 	}
 	
+	public static String getStringContentOfLastCommit(String filePath,Repository repository) throws RevisionSyntaxException, AmbiguousObjectException, IncorrectObjectTypeException, IOException {
+		// find the HEAD
+		ObjectId lastCommitId = repository.resolve(Constants.HEAD);
+
+		// a RevWalk allows to walk over commits based on some filtering that is
+		// defined
+		RevWalk revWalk = new RevWalk(repository);
+		RevCommit commit = revWalk.parseCommit(lastCommitId);
+		// and using commit's tree find the path
+		RevTree tree = commit.getTree();
+		System.out.println("Having tree: " + tree);
+
+		// now try to find a specific file
+		TreeWalk treeWalk = new TreeWalk(repository);
+		treeWalk.addTree(tree);
+		treeWalk.setRecursive(true);
+		treeWalk.setFilter(PathFilter.create(filePath));
+		if (!treeWalk.next()) {
+			
+			//TODO the file is added to project
+			throw new IllegalStateException(
+					"CHANGECOMMIT -- Did not find expected file '" + filePath + "'");
+		}
+
+		ObjectId objectId = treeWalk.getObjectId(0);
+		ObjectLoader loader = repository.open(objectId);
+		
+		StringWriter writer = new StringWriter();
+
+		return IOUtils.stringFromFile(Utils.inputStreamToFile(loader.openStream()).getAbsolutePath());
+	}
+
 	public static File inputStreamToFile(InputStream is) throws IOException {
 		File contentFile = File.createTempFile("tmpCont", ".txt");
 
